a4fb9e4884132cba317d1bdcc9797747aa1ef94a
_eyewave <89079979+eye-wave@users.noreply.github.com>
2023-06-19 00:06:48 +0200

    migrate to esbuild

--- lib/parser.d.ts
+++ lib/parser.d.ts
@@ -1,5 +1,5 @@
 export declare function parseCSV(input: string, customTypes?: CustomTypeDefinition[]): any[];
-export type CustomTypeDefinition = {
+export declare type CustomTypeDefinition = {
     name: string;
     parse: (input: string) => unknown;
 };
